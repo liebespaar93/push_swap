8b47d1eb214a2b52d30109d30b3d6dc1f10afebc
MacBook Air <liebespaar93@gmail.com>
2022-08-13 09:03:01 +0900

    2022/08/13 error fix

--- src_quick_sort/ft_pivot_split.c
+++ src_quick_sort/ft_pivot_split.c
@@ -6,63 +6,13 @@
 /*   By: kyoulee <kyoulee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/12 15:31:02 by kyoulee           #+#    #+#             */
-/*   Updated: 2022/08/13 06:52:17 by kyoulee          ###   ########.fr       */
+/*   Updated: 2022/08/13 09:00:01 by kyoulee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 #include <ft_quick_sort.h>
-
-#include <stdio.h>
-int ft_print_stack(t_d_list *a, t_d_list_header **pivot, char *str)
-{
-	t_d_list *print;
-	int bb;
-
-	char *Y = "\e[103m";
-	char *B = "\e[104m";
-	char *DG = "\e[100m";
-	char *NO = "\e[0m";
-	char *NG = "\033[105m";
-	printf("\e[38;5;196m %s : \t", str);
-	printf("\n\n");
-	if (!a && printf("\e[100m NULL \e[0m\n\n"))
-		return (0);
-	print = a;
-	bb = 0;
-	 printf("\e[100m");
-	while (print)
-	{
-		if (pivot[0] && pivot[0]->head && pivot[0]->head->data == print->data)
-	 		printf("%s", Y);
-		if (pivot[1] && pivot[1]->head && pivot[1]->head->data == print->data)
-			printf("%s", NG);
-		if (pivot[2] && pivot[2]->head && pivot[2]->head->data == print->data)
-			printf("%s", B);
-		if (pivot[3] && pivot[3]->head && pivot[3]->head->data == print->data)
-			printf("%s", B);
-		if (pivot[4] && pivot[4]->head && pivot[4]->head->data == print->data)
-			printf("%s", B);
-		printf("  %d  \t", *(int *)print->data);
-		printf("%s", DG);
-		print = print->next;
-		bb++;
-		if (print && (print->data == a->data || bb > 10))
-			break;
-	}
-	printf("\e[0m\n\n");
-	return (1);
-}
-
-void ft_print_ab(t_d_list_header *a, t_d_list_header *b, t_d_list_header **pivot)
-{
-	printf("\n ==================\n");
-	ft_print_stack(a->head, pivot,"a : ");
-	ft_print_stack(b->head, pivot,"b : ");
-	printf("\n ==================\n");
-	sleep(1);
-}
-
+#include <ft_push_swap.h>
 
 t_d_list	*ft_pivot_split(t_d_list_header *a, t_d_list_header *b, \
 	t_d_list_header **pivot, int deep)
@@ -82,7 +32,6 @@ t_d_list	*ft_pivot_split(t_d_list_header *a, t_d_list_header *b, \
 	}
 	while (b->head)
 		ft_pa(a, b);
-	ft_print_ab(a,b, pivot);
 	return (a->head);
 }
 
@@ -115,7 +64,7 @@ int	ft_pivot_split_b_left(t_d_list_header *a, t_d_list_header *b, \
 	check = (int [2]){0, 0};
 	pivot[++(*deep)]->head = b->head;
 	if (!a->head)
-		pivot[1] = pivot[*deep];
+		ft_memcpy(pivot[1], pivot[*deep], sizeof(t_d_list_header));
 	if (b->head && ft_pa(a, b))
 		ft_ra(a);
 	while (b->head && pivot[0]->head != b->head)
@@ -146,14 +95,14 @@ int	ft_pivot_split_a_right(t_d_list_header *a, t_d_list_header *b, \
 	pivot_data = *pivot[*deep];
 	pivot[*deep]->head = a->tail;
 	if (a->tail && ft_rra(a) && ft_pb(a, b) && ft_rb(b))
-		while (a->tail && pivot_data.tail != a->tail && ++check[0])
+		while (a->tail && pivot_data.head != a->tail && ++check[0])
 			if (ft_rra(a) && ft_pb(a, b))
 				if (*(pivot[*deep]->head->data) < *b->head->data)
 					check[1] += ft_rb(b);
 	if (a->tail && pivot_data.head == a->tail && ft_rra(a))
 		ft_pb(a, b);
 	pivot[*deep]->tail = b->tail;
-	if (a->head)
+	if (!a->head)
 		ft_bzero(pivot[1], sizeof(t_d_list_header));
 	return (check[0] - check[1]);
 }
@@ -174,7 +123,7 @@ int	ft_pivot_split_b_right(t_d_list_header *a, t_d_list_header *b, \
 	pivot_data = *pivot[*deep];
 	pivot[*deep]->head = b->tail;
 	if (!a->head)
-		pivot[1] = pivot[*deep];
+		ft_memcpy(pivot[1], pivot[*deep], sizeof(t_d_list_header));
 	if (b->tail && ft_rrb(b) && ft_pa(a, b))
 		ft_ra(a);
 	while (b->tail && pivot_data.head != b->tail && ++check[0] && ft_rrb(b))
--- src_quick_sort/ft_quick_sort.c
+++ src_quick_sort/ft_quick_sort.c
@@ -6,7 +6,7 @@
 /*   By: kyoulee <kyoulee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/12 15:31:02 by kyoulee           #+#    #+#             */
-/*   Updated: 2022/08/13 05:52:50 by kyoulee          ###   ########.fr       */
+/*   Updated: 2022/08/13 08:53:59 by kyoulee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,27 @@
 
 t_d_list	*ft_quick_sort(t_d_list *memory, int max_len)
 {
+	t_d_list		*quick_sort;
 	t_d_list_header	*a;
 	t_d_list_header	*b;
 	t_d_list_header	**pivot;
 	int				deep;
 
-	deep = 1;
+	deep = 0;
 	if (!ft_zeromalloc((void **)&pivot, sizeof(t_d_list_header *) * max_len))
 		return (NULL);
-	while (max_len--)
-		if (!ft_zeromalloc((void **)&pivot[max_len], sizeof(t_d_list_header)))
+	while (deep < max_len)
+		if (!ft_zeromalloc((void **)&pivot[deep++], sizeof(t_d_list_header)))
 			return (NULL);
+	deep = 1;
 	a = ft_d_list_header_init(memory);
 	b = ft_d_list_header_init(NULL);
 	pivot[0]->head = a->head;
-	return (ft_pivot_split(a, b, pivot, deep));
+	quick_sort = ft_pivot_split(a, b, pivot, deep);
+	while (max_len--)
+		ft_d_list_free(pivot[max_len], free);
+	free(pivot);
+	free(a);
+	free(b);
+	return (quick_sort);
 }
